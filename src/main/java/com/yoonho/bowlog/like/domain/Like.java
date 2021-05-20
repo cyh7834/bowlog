bea0e481f96a08df8d55cd8f394ec37e260f2ae7
choiyoonho <cyh7834@gmail.com>
2021-05-20 22:00:48 +0900

    Like 엔티티 추가

---
+++ src/main/java/com/yoonho/bowlog/like/domain/Like.java
@@ -0,0 +1,31 @@
+package com.yoonho.bowlog.like.domain;
+
+import com.yoonho.bowlog.post.domain.Post;
+import com.yoonho.bowlog.reply.domain.Reply;
+import com.yoonho.bowlog.user.domain.Account;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "POST_LIKE")
+public class Like {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="account_id")
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id")
+    private Reply reply;
+}
